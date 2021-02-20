eee95f60535355b8b95bdc01116ee6c1d3dbb2b6
darshan207 <darshanparmar272002@gmail.com>
2021-02-20 18:28:44 +0530

    commited by team #2

--- EasyToQuiz/userlogin/urls.py
+++ EasyToQuiz/userlogin/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
-# from . import views
+from . import views
 from django.conf.urls import url
-from .views import index
+from .views import login, userregistration
 urlpatterns=[
 
-    url(r'^index/$', index),
+    url(r'^login/$', login),
+    path('login', views.login, name= 'login'),
+    url(r'^userregistration/$', userregistration),
+    path('userregistration', views.userregistration, name= 'userregistration'),
+    
     # path('UserRegistration',views.register,name='register')
 ]
--- EasyToQuiz/userlogin/views.py
+++ EasyToQuiz/userlogin/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.views import generic
+from django.template.context_processors import csrf
+from .models import user_signup
+from django.contrib.auth.models import User, auth
+import mysql.connector
+from django.contrib import messages
 # Create your views here.
-def index(request):
-    return render(request, "index.html")
+
+def login(request):
+    if request.method == 'POST':
+        sname = request.POST.get('username', '')
+        spass = request.POST.get('password', '')
+        user = auth.authenticate(username=sname,password=spass)
+        if user is not None:
+            messages.success(request,"Login Successfull!!!")
+            return render(request, "login.html")
+        else:
+            messages.error(request,"Username or password invalid")
+            return render(request,'login.html')
+    else:
+        return render(request, "login.html")
+
+def userregistration(request):
+    if request.method == 'POST':
+        sname = request.POST.get('username', '')
+        semail = request.POST.get('email', '')
+        spass = request.POST.get('pass', '')
+        spass1 = request.POST.get('cpass','')
+        if spass==spass1:
+            if User.objects.filter(username=sname).exists():
+                messages.error(request,"username already exist.");
+                return render(request, "userregistration.html")
+            elif User.objects.filter(email=semail).exists():
+                messages.error(request,"This email is already connected to an account.")
+                return render(request, "userregistration.html")
+            else:
+                messages.success(request,"Register Successfull, please Login again for confirmation.")
+                s = User.objects.create_user(username = sname, email=semail, password=spass)
+                s.save()
+                return HttpResponseRedirect('/userlogin/login')
+        else:
+            messages.error(request,"confirm password is not same as password! please check!")
+            return render(request, "userregistration.html")
+    else:
+        return render(request, "userregistration.html")
+
