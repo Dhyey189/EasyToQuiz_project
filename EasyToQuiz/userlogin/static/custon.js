437cd0e4258e52f3b8edc0b3661909d2b2772bf0
divyesh90 <divyeshpatel908137@gmail.com>
2021-03-06 20:33:17 +0530

    commited by team #4

---
+++ EasyToQuiz/userlogin/static/custon.js
@@ -0,0 +1,89 @@
+var x = 0;
+
+var array = new Array();
+var array2 = new Array();
+array[0] = 0;
+array2[0] = 0;
+
+function AddQuestion() {
+	x++;
+	array[x] = 0;
+	array2[x] = 1;
+	var mainsection = document.createElement("section");
+	mainsection.setAttribute("class", "mysection2");
+	mainsection.setAttribute("ID", x + "mysection");
+	var intersection = document.createElement("div");
+	intersection.setAttribute("class", "container");
+	var section = document.createElement("div");
+	section.setAttribute("class", "section questionsection");
+	var Item = document.createElement("div");
+	Item.setAttribute("class", "item option-item");
+	Item.setAttribute("ID", "item" + x);
+	var inputID = document.createElement("INPUT");
+	inputID.setAttribute("type", "text");
+	inputID.setAttribute("name", "QuestionTitle");
+	inputID.setAttribute("placeholder", "Question-" + x);
+	var Item2 = document.createElement("div");
+	Item2.setAttribute("class", "item delete");
+	Item2.setAttribute("align", "center");
+	var Delete = document.createElement("a");
+	Delete.innerHTML = "Delete";
+	Delete.setAttribute("class", "btn");
+	Delete.setAttribute("name", "deletebtn");
+	Delete.setAttribute("ID", x)
+	Delete.setAttribute("value", "Delete");
+	Delete.setAttribute("onclick", "Delete(getAttribute(\"ID\"))");
+	var addoption = document.createElement("a");
+	addoption.innerHTML = "<i class=\"fas fa-plus-circle\" title=\"Add Option\"></i>";
+	addoption.setAttribute("class", "btnicon addoption");
+	addoption.setAttribute("name", "addoptionbtn");
+	addoption.setAttribute("ID", x);
+	addoption.setAttribute("onclick", "AddOption(getAttribute(\"ID\"))");
+	Item.append(inputID);
+	Item.append(addoption);
+	Item2.append(Delete);
+	section.append(Item);
+	section.append(Item2);
+	intersection.append(section);
+	mainsection.append(intersection);
+	
+	document.getElementsByTagName("body")[0].appendChild(mainsection);
+	RemoveElement();
+}
+
+function Delete(y) {
+	array2[y] = 0;
+	var DeleteQus = document.getElementById(y + "mysection");
+	DeleteQus.parentNode.removeChild(DeleteQus);
+	RemoveElement();
+}
+
+function AddOption(y) {
+	array[y]++;
+	var div = document.createElement("div");
+	div.setAttribute("class", "option");
+	div.setAttribute("Id", "div-" + y + "-" + array[y]);
+	var optionID = document.createElement("INPUT");
+	optionID.setAttribute("type", "text");
+	optionID.setAttribute("name", "option");
+	optionID.setAttribute("placeholder", "option " + array[y]);
+	optionID.setAttribute("ID", y + "-" + array[y]);
+	var deleteoption = document.createElement("a");
+	deleteoption.innerHTML = "<i class=\"fas fa-minus-circle\" title='Delete Option-" + array[y] + "'></i>";
+	deleteoption.setAttribute("class", "btnicon-3 deleteoption");
+	deleteoption.setAttribute("name", "deleteoptionbtn");
+	deleteoption.setAttribute("ID", y + "-" + array[y]);
+	deleteoption.setAttribute("onclick", "DeleteOption(getAttribute(\"ID\"))");
+	div.append(optionID);
+	div.append(deleteoption);
+	document.getElementById("item" + y).appendChild(div);
+}
+
+function DeleteOption(a) {
+	var DeleteOpt = document.getElementById("div-" + a);
+	DeleteOpt.parentNode.removeChild(DeleteOpt);
+}
+
+window.onbeforeunload = function (event) {
+	return confirm("Confirm refresh");
+};
